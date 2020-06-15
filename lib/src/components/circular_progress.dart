ade8d604cb5b8abf2c374e6796b179d0616ee009
“Jose <pintojose.benedicto2@gmail.com>
2020-06-14 18:46:18 -0600

    Added some components

---
+++ lib/src/components/circular_progress.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CircularProgress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/components/linear_progress.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LinearProgress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
